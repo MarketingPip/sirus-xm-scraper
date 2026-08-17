4c9874c540aa228861333783c823a464960c05fb
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-08-17 00:50:55 -0400

    Update scrapeChannels.py

--- scrapeChannels.py
+++ scrapeChannels.py
@@ -98,7 +98,7 @@ def create_session(
         ),
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
